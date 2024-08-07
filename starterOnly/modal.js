aff1026b3a8a0ff63b59f2708eef526c83153ed2
gouttebroze <docteurfutur@gmail.com>
2024-08-07 19:59:22 +0200

    check input form values

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,35 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+/******************************
+ *  check inputs form values 
+ ******************************/
 
+let checkbox1 = document.getElementById("checkbox1");
+let radioBtnList = document.querySelectorAll('input[type=radio]');
+
+/*** eventListeners ***/ 
+// check firstName input value (on "change" event)
+// firstName.addEventListener("change", () => {
+//   console.log(firstName.value);
+// })
+
+// check checkbox value
+console.log(checkbox1.checked);
+
+// check radio buttons values
+for (let i = 0; i < radioBtnList.length; i++) {
+  if (radioBtnList[i].checked) {
+    console.log(radioBtnList[i].value);
+  }
+}
+
+
+let form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // wanna control submission
+
+  // check firstName input value
+  let firstName = document.querySelector("#first");
+  console.log(firstName.value);
+})
